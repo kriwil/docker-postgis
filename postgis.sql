3f79841ed8742f3dad683f0f4a67656f181ac457
Aldiantoro Nugroho <kriwil@gmail.com>
2015-10-13 20:27:47 +0700

    use postgres:9.4, add custom sql script

---
+++ postgis.sql
@@ -0,0 +1,6 @@
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION postgis_tiger_geocoder;
+CREATE EXTENSION unaccent;
+ALTER FUNCTION unaccent(text) IMMUTABLE;
